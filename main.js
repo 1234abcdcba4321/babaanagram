b88d565976910d64929013fe34007cf7751cf242
Slabdrill <1234abcdcba4321@users.noreply.github.com>
2021-04-13 15:47:59 -0500

    update to v2401 also adds localstorage wordlist saving

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 "use strict"
 
-function test() {
+function calculate() {
   let letters = {};
   let singleL = document.getElementById("letters1").value.toLowerCase();
   //console.log(document.getElementById("letters1"));
   for (let i=0;i<singleL.length;i++) {
-      if (!letters[singleL[i]]) letters[singleL[i]] = 1;
-      else letters[singleL[i]]++;
+    if (!letters[singleL[i]]) letters[singleL[i]] = 1;
+    else letters[singleL[i]]++;
   }
 
   const hasMultiLetters = document.getElementById("letters2").value.length > 1;
@@ -48,4 +48,13 @@ function test() {
   }
 
   document.getElementById("output").value = ret;
+}
+
+
+const defaultList = "algae arrow baba banana bat bee belt bird blob boat bog bolt book box brick bubble bucket bug burger cake cart cash circle cliff cloud cog crab crystal cup door dot drink dust ear eye fence fire fish flag flower fofo foliage foot fort fruit fungi fungus gate gem ghost grass guitar hand hedge hihat house husk husks ice it jelly jiji keke key ladder lamp lava leaf lift line lizard love me monitor moon nose orb pants piano pillar pipe pixel plane planet pumpkin reed ring road robot rock rocket rose rubble sax seed shell shirt shovel sign skull spike sprout square star statue stick stump sun tile tower track train tree trees triangle turnip ufo vine wall water wind worm  all text level group group2 group3 empty not is and has eat fear follow make mimic play auto right up left down red orange yellow lime green cyan blue purple pink rosy black grey silver white brown best sad wonder party broken chill defeat back fall fallright fallup fallleft nudgedown nudgeright nudgeup nudgeleft turn deturn float phantom hide hot melt more move open shut powered power pull push safe shift sink sleep still lockeddown lockedright lockedup lockedleft stop swap tele weak win word you you2 bonus end done revert idle lonely often seldom on near facing without above below feeling cursor select  sharp flat anni crash error never edge blossom 3d";
+
+function loadList() {
+  let item = localStorage.getItem('wordlist')
+  if (!item) item = defaultList;
+  document.getElementById("words").value = item
 }
